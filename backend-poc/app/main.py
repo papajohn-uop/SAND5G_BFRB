d9696cffe0a31eaa5e445ba0713dad90b397def0
Panagiotis Papapaionou <papajohn@upatras.gr>
2026-09-08 08:31:28 +0000

    CORS

--- backend-poc/app/main.py
+++ backend-poc/app/main.py
@@ -2,6 +2,7 @@ import random
 from typing import List
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 
 
@@ -25,6 +26,14 @@ class CheckCVEResponse(BaseModel):
 
 
 app = FastAPI(title="SAND5G BRFB PoC API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def build_random_response(cve: str) -> CheckCVEResponse:
